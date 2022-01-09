5af623ee31af94859f8c08d71a27e2dc45e30004
aexer0e <aexeroe@gmail.com>
2022-01-09 04:44:53 +0300

    ai player

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const words = require('an-array-of-english-words').filter(e => e.length == 5).map(e => e.toUpperCase());
-const { createCipheriv } = require('crypto');
 const readline = require('readline')
 //wordle game
 class Game {
@@ -8,14 +7,15 @@ class Game {
     guessCount = 0;
     maxGuesses = 0;
     gameOver = false;
+    won = false;
 
-    constructor() {
-        this.#word = words[Math.floor(Math.random() * words.length)]
+    constructor(word) {
+        this.#word = word || words[Math.floor(Math.random() * words.length)]
         this.guesses = []
         this.guessCount = 0
         this.maxGuesses = 5
         this.gameOver = false
-        // console.log(this.#word)
+        this.won = false
     }
 
     guess(guessedWord) {
@@ -28,7 +28,7 @@ class Game {
             word: guessedWord,
             correct: this.#word == guessedWord,
             letters: [],
-            guessCount: this.guessCount,
+            guessCount: this.guessCount
         }
 
         for (let i = 0; i < 5; i++) {
@@ -41,41 +41,95 @@ class Game {
 
         this.guesses.push(guess)
         
+        if (guessedWord == this.#word) {
+            this.gameOver = true
+            this.won = true
+            return `Correct! You won with ${this.guessCount} guesses!`
+        }
+
         if (this.guessCount >= this.maxGuesses) {
             this.gameOver = true
+            return `You lost! The word was ${this.#word}`
         }
 
         return guess
     }
 }
 
-async function play() {
+async function play(initWord = 'hello', pickedWord = null) {
     const chalkImp = await import('chalk')
     const chalk = new chalkImp.Chalk()
-    const myGame = new Game()
+    const myGame = new Game(pickedWord)
 
     const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+       input: process.stdin,
+       output: process.stdout
     })
+
+    let letterBlacklists = [ new Set(), new Set(), new Set(), new Set(), new Set() ]
+    let includedLetters = new Set()
+    let possibleWords = [...words]
+    let knownLetters = []
 
     while (!myGame.gameOver) {
         const guessesLeft = myGame.maxGuesses - myGame.guessCount
-        const guess = await new Promise(resolve => rl.question(guessesLeft + ' guesses left. Guess a word: ', resolve))
+        // const guess = await new Promise(resolve => rl.question(guessesLeft + ' guesses left. Guess a word: ', resolve))
+        const guess = myGame.guessCount == 0 ? initWord : possibleWords[Math.floor(Math.random() * possibleWords.length)]
+        console.log('guessing ' + guess)
         const guessResult = myGame.guess(guess)
         if (typeof guessResult === 'string') {
-            console.log(chalk.red(guessResult))
+            if (guessResult.startsWith('Correct!')) console.log(chalk.green(guessResult))
+            else console.log(chalk.red(guessResult))
             continue
         }
-        const result = guessResult.letters.map(e => {
+        const resultColored = guessResult.letters.map(e => {
             if (e.correct) return chalk.green(e.letter)
             if (e.contained) return chalk.yellow(e.letter)
             return chalk.red(e.letter)
         })
-        console.log(result.join(' '))
+        console.log(resultColored.join(' '))
+
+        wrongLetters = guessResult.letters.filter(e => !e.correct && !e.contained).map(e => e.letter)
+
+        for (let i = 0; i < 5; i++) {
+            const letter = guessResult.letters[i]
+
+            wrongLetters.forEach(e => { letterBlacklists[i].add(e) })
+
+            if (letter.contained) {
+                includedLetters.add(letter.letter)
+                if (letter.correct) knownLetters[i] = letter.letter
+                else {letterBlacklists[i].add(letter.letter)}
+            }
+        }
+        possibleWords = possibleWords.filter(e => {
+            for (let i = 0; i < 5; i++) { 
+                if (knownLetters[i] && knownLetters[i] != e[i]) return false
+                if (letterBlacklists[i].has(e[i])) return false
+            }
+            for (let i = 0; i < includedLetters.size; i++) {
+                if (!e.includes([...includedLetters][i])) return false
+            }
+            return true
+        })
+        if (possibleWords.length > 50) console.log(possibleWords.length + ' possible words')
+        else console.log(possibleWords.length + ' possible words: ' + possibleWords.join(', '))
     }
 
     rl.close()
+    return myGame.won
 }
-
 play()
+function main() {
+    let wordsWinrate = {}
+    for (let i = 0; i < words.length; i++) {
+       let wins = 0
+       for (let y = 0; y < words.length; y++) {
+           const won = play(words[i], words[y])
+           if (won) wins++
+       }
+       wordsWinrate[words[i]] = wins / words.length
+       console.log(words[i] + ': ' + wins + '/' + words.length + ' (' + (wins / words.length * 100) + '%)' + ` (${i}/${words.length})`)
+    }
+    console.log(JSON.stringify(wordsWinrate))
+}
